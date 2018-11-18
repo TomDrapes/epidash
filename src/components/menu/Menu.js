08aeb3a6fb8d7f251a38311102948721c39e85cd
Tom <tjdrapes@hotmail.com>
2018-11-18 15:51:18 +1000

    created menu

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import './menu.css'
+
+export default class Menu extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            menuItems: [
+                {priceCompare: true},
+                {suggestions: false},
+                {competition: false},
+                {inventory: false},
+                {taxRecords: false},
+            ]
+        }
+
+    }
+
+    updateSelected(menuItem){
+        let updatedMenu = this.state.menuItems.map(item => {
+            if (menuItem in item){
+                return { [Object.keys(item)[0]] : true }
+            }
+            return { [Object.keys(item)[0]]: false}
+        })
+        this.setState({ menuItems: updatedMenu })        
+    }
+
+    render(){
+        let priceCompare = this.state.menuItems[0].priceCompare && "selected"
+        let suggestions = this.state.menuItems[1].suggestions && "selected"
+        let competition = this.state.menuItems[2].competition && "selected"
+        let inventory = this.state.menuItems[3].inventory && "selected"
+        let taxRecords = this.state.menuItems[4].taxRecords && "selected"
+
+        return(
+            <div className="menu-container">
+                <img className='logo' src={require("../../images/hawk_logo.png")} alt='logo' />
+                <ul className="menu-items">
+                    <li className={priceCompare} onClick={() => this.updateSelected('priceCompare')}>Price Compare</li>
+                    <li className={suggestions} onClick={() => this.updateSelected('suggestions')}>Suggestions</li>
+                    <li className={competition} onClick={() => this.updateSelected('competition')}>Competition</li>
+                    <li className={inventory} onClick={() => this.updateSelected('inventory')}>Inventory</li>
+                    <li className={taxRecords} onClick={() => this.updateSelected('taxRecords')}>Tax Records</li>
+                </ul>
+            </div>
+        )
+    }
+}
---
+++ src/components/price_compare/PriceCompare.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+export default class PriceCompare extends Component {
+
+    constructor(props){
+        super(props)
+        
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="price-compare-container">
+            </div>
+        )
+    }
+}
