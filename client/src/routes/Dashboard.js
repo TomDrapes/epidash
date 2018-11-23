d26570fd691e1004d38eca4b3553e713771f97aa
Tom <tjdrapes@hotmail.com>
2018-11-23 21:30:10 +1000

    created some initial routes and designed the login form

--- client/src/routes/Dashboard.js
+++ client/src/routes/Dashboard.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import Menu from '../components/menu/Menu'
 import SideBar from '../components/side_bar/SideBar'
 import PriceCompare from '../components/price_compare/PriceCompare'
+import './style.css'
 
 export default class DashBoard extends Component{
 
     render(){
         return(
-            <div>
+            <div className='dashboard'>
                 <Menu />
                 <SideBar />
                 <PriceCompare />
--- client/src/routes/LoginPage.js
+++ client/src/routes/LoginPage.js
@@ -9,8 +9,31 @@ export default class LoginPage extends Component{
 
     render(){
         return (
-            <div>
-                <form>Login</form>
+            <div className='login-page'>
+                <div className='login-container'>
+                    <h1>EpiDash</h1>
+                    <form>
+                        <h2>USERNAME</h2>
+                        <i class="fas fa-user"></i>
+                        <input type='text' placeholder='Username/Email'></input><br/>
+                        <h2>PASSWORD</h2>
+                        <i class="fas fa-lock"></i>
+                        <input type='text' placeholder='Password' /><br />
+                        <div className='remember-forgot-container'>
+                            <div className='remember-me'>
+                                <input type='checkbox' />Remember me
+                            </div>
+                            <div className='forgot-password'>
+                                <a href="">I forgot my password</a>
+                            </div>
+                        </div>
+                    </form>
+                    <div>
+                        
+                        <hr />
+                        <p>Not a member yet? <a href="">JOIN</a></p>
+                    </div>
+                </div>
             </div>
         )
     }
