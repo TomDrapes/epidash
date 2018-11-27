38c6627cb30456cd87378b12bb8f7f9b0ff3c9f4
Tom Drapes <tjdrapes@gmail.com>
2018-11-27 21:44:12 +1000

    created sign up page shell

---
+++ client/src/routes/SignUpPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+export default class SignUpPage extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {}
+  }
+
+  render(){
+    return (
+      <div className="signup-page-container">
+
+      </div>
+    )
+  }
+}
