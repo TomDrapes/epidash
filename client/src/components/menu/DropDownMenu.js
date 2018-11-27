992299ff95868819c6b0f65071f4e2dbb5e9537d
Tom Drapes <tjdrapes@gmail.com>
2018-11-27 21:24:51 +1000

    initial component setup for drop down menus

---
+++ client/src/components/menu/DropDownMenu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './style.css'
+
+export default class DropDownMenu extends Component {
+
+  render(){
+    return (
+      <div className='dropdown-container'>
+      </div>
+    )
+  }
+}
