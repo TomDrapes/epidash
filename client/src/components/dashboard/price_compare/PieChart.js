05a2fe6ac4e22e1fcd23c7c36fcde74e1e4fa6f6
Tom Drapes <tjdrapes@gmail.com>
2018-12-10 15:44:50 +1000

    modified pie chart to be scalable

--- client/src/components/dashboard/price_compare/PieChart.js
+++ client/src/components/dashboard/price_compare/PieChart.js
@@ -6,16 +6,16 @@ export default class PieChart extends Component {
         super(props)
 
         this.state = {
-            width: 175,
-            height: 175,
+            width: 100,
+            height: 100,
         }
     }
 
     slice= (pie) =>{
 
         let arc = d3.arc()
-            .innerRadius(50)
-            .outerRadius(80)
+            .innerRadius(this.state.width/5)
+            .outerRadius(this.state.width/2)
 
         //let interpolate = d3.interpolateRgb('#eaaf79', '#33bc51')
         let colors = ['rgb(243, 195, 92)', 'rgb(33, 182, 245)', 'rgb(231, 69, 69)', 'rgb(97, 202, 97)']
@@ -28,15 +28,16 @@ export default class PieChart extends Component {
     }
 
     render() {
-        let pie = d3.pie()(this.props.data)
-        return (
-            <div>
-                <svg height={this.state.height} width={this.state.width}>
-                    <g transform={`translate(${this.state.width/2}, ${this.state.height/2})`}>
-                        {this.slice(pie)}
-                    </g>
-                </svg>            
-            </div>
-        )
+      let pie = d3.pie()(this.props.data)
+
+      return (
+        <div className='pie-container'>
+            <svg viewBox={`0 0 ${this.state.width} ${this.state.height}`} preserveAspectRatio="xMidYMid meet" >
+                <g transform={`translate(${this.state.width/2}, ${this.state.height/2})`}>
+                    {this.slice(pie)}
+                </g>
+            </svg>
+        </div>
+      )
     }
 }
