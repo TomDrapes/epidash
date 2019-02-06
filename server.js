7556ddccba118e099cf2eba134708865097969b6
Tom <tjdrapes@hotmail.com>
2019-02-06 16:39:05 +1000

    forgot to require createServer from http

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
+const {createServer} = require('http')
 
 //Modules needed for file uploads
 const fileUpload = require('express-fileupload')
