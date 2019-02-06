0c40ef7adcbdb0ce357caefb85d298d3fa2235d9
Tom <tjdrapes@hotmail.com>
2019-02-06 16:32:01 +1000

    path name changed again

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use(cors())
 app.use(fileUpload())
 
 //Added for heroku deployment
-app.use(express.static(path.resolve(__dirname, 'client/build')))
+app.use(express.static(path.resolve(__dirname, '/client/build')))
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'))
 })
 
 //Socket.io
